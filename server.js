806ffdb65082aefa0a056487e0b3ed3fe1657dbc
marques-j-robinson <marques.j.robinson@gmail.com>
2021-08-17 12:13:29 -0700

    Post Message Service
    - Install body-parser for JSON parsing
    - Create a POST /message endpoint
    - Add a function to frontend that binds to click event of send button
        - Requests the POST /message endpoint
        - Invokes addMessage to display the message on the frontend

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const messages = [
+    {name: "Tim", message: "yo"},
+    {name: "Pam", message: "hi"}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+})
+
+app.post('/message', (req, res) => {
+    messages.push(req.body);
+    res.sendStatus(200);
+})
 
 const server = app.listen(3000, () => {
     const {port} = server.address();
